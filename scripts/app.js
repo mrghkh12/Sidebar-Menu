33779bf5345c8eff5f44765660c89205b4035887
mohammad reza <mrghkh12@gmail.com>
2024-08-03 21:55:33 +0330

    feat(app.js): set event for active list items

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const $ = document
 
 const sidebarContainer = $.querySelector('.sidebar')
 const sidebarBottom = $.querySelector('.sidebar-bottom')
+const listItems = $.querySelectorAll('.list-item')
 
 sidebarBottom.addEventListener('click' , e =>{
     if(e.target.classList.contains('dark')){
@@ -15,3 +16,13 @@ sidebarBottom.addEventListener('click' , e =>{
         sidebarContainer.className = 'sidebar light'
     }
 })
+
+
+function activeItem(){
+    listItems.forEach(item => item.classList.remove('active'))
+
+    this.classList.add('active')
+}
+
+
+listItems.forEach(item => item.addEventListener('click' , activeItem))
