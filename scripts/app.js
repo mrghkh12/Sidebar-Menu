01b43fa8a8c41cd287c423445f5ad9b697fe6979
mohammad reza <mrghkh12@gmail.com>
2024-08-03 21:56:29 +0330

    style: clean codes

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,25 @@
+const $ = document;
 
-const $ = document
+const sidebarContainer = $.querySelector(".sidebar");
+const sidebarBottom = $.querySelector(".sidebar-bottom");
+const listItems = $.querySelectorAll(".list-item");
 
-const sidebarContainer = $.querySelector('.sidebar')
-const sidebarBottom = $.querySelector('.sidebar-bottom')
-const listItems = $.querySelectorAll('.list-item')
+sidebarBottom.addEventListener("click", (e) => {
+  if (e.target.classList.contains("dark")) {
+    sidebarContainer.className = "sidebar";
+  }
+  if (e.target.classList.contains("night")) {
+    sidebarContainer.className = "sidebar night";
+  }
+  if (e.target.classList.contains("light")) {
+    sidebarContainer.className = "sidebar light";
+  }
+});
 
-sidebarBottom.addEventListener('click' , e =>{
-    if(e.target.classList.contains('dark')){
-        sidebarContainer.className = 'sidebar'
-    }
-    if(e.target.classList.contains('night')){
-        sidebarContainer.className = 'sidebar night'  
-    }
-    if(e.target.classList.contains('light')){
-        sidebarContainer.className = 'sidebar light'
-    }
-})
+function activeItem() {
+  listItems.forEach((item) => item.classList.remove("active"));
 
-
-function activeItem(){
-    listItems.forEach(item => item.classList.remove('active'))
-
-    this.classList.add('active')
+  this.classList.add("active");
 }
 
-
-listItems.forEach(item => item.addEventListener('click' , activeItem))
+listItems.forEach((item) => item.addEventListener("click", activeItem));
